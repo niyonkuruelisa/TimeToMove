0760c973c9d4e92c59081b88746dc69dfb4f8959
Niyonkuru Elisa <niyonkuruelisa@gmail.com>
2022-01-17 18:42:22 +0200

    First commit, empty project and .gitignore

---
+++ Assets/Scripts/Menu/UI_NavigationScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_NavigationScript : MonoBehaviour
+{
+
+
+
+    //LEVELS SCENES
+
+    //Level Menu
+    // MENU BUTTON - Load MENU Scene
+    public void OnMenuButtonClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    // LEVEL 1 BUTTON - Load LEVEL 1 Scene
+    public void OnLevel1ButtonClicked()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+
+    //Main Menu
+    // PLAY BUTTON - Load Level Scene
+    public void OnPlayButtonClicked()
+    {
+        SceneManager.LoadScene("MenuLevels");
+    }
+
+    // SETTINGS BUTTON - Load SETTINGS Scene
+    public void OnSettingsButtonClicked()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    // HELP BUTTON - Load HELP Scene
+    public void OnHelpButtonClicked()
+    {
+        SceneManager.LoadScene("Help");
+    }
+
+    // QUIT BUTTON - CLOSE GAME
+    public void OnQuitButtonClicked()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            Application.Quit();
+        }
+    }
+}
